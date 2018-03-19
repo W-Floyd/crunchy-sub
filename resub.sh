3daa3c086a80ed3e08ec04a675ad4d4ec0fc83e5
William Floyd <william.png2000@gmail.com>
2018-03-19 00:40:46 -0400

    resub: Now working, confirmed on Golden Time.

--- resub.sh
+++ resub.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+until [ "${#}" = '0' ]; do
+    export "${1}"
+    shift
+done
+
 while read -r __program; do
     if ! which "${__program}" &> /dev/null; then
         echo "'${__program}' not installed."
@@ -8,6 +13,14 @@ while read -r __program; do
 done <<< 'awk-csv-parser'
 
 source ./crunchyroll
+
+__pushd () {
+    pushd "${1}" &> /dev/null
+}
+
+__popd () {
+    popd &> /dev/null
+}
 
 # modified from https://github.com/xvoland/Extract
 extract () {
@@ -54,14 +67,14 @@ if ! [ -d "${1}" ]; then
 fi
 }
 
-# list_subs 'sub name'
+# list_subs 'Sub Name'
 list_subs () {
     local __sub_title="${1}"
     local __url="$(grep "<strong>${__sub_title}</strong>" < "${__site_file}" | sed -e 's#.*<a href="#http://kitsunekko.net#' -e 's/" class=.*//')"
     wget "${__url}" -qO - | grep '<tr><td>' | sed -e 's#.*<a href="#http://kitsunekko.net/#' -e 's/" class=.*//'
 }
 
-# download_subs 'sub name'
+# download_subs 'Sub Name'
 download_subs () {
     local __sub_title="${1}"
     local __sub_dir="${__download_dir_subs}/${__sub_title}"
@@ -72,19 +85,19 @@ download_subs () {
         __mkdir "${__url_dir}"
         if [ -z "$(ls -A "${__url_dir}")" ]; then
             wget --directory-prefix="${__url_dir}" "${__url}"
-            pushd "${__url_dir}"
+            __pushd "${__url_dir}"
             # I know, it'll only ever be one file, but old habits die hard...
             find ./ | while read -r __file; do
                 extract "${__file}"
             done
             
-            popd
+            __popd
             
         fi
     done
 }
 
-# list_downloaded_subs 'sub name' 'hash'
+# list_downloaded_subs 'Sub Name' 'hash'
 list_downloaded_subs () {
     local __sub_title="${1}"
     local __sub_hash="${2}"
@@ -93,29 +106,110 @@ list_downloaded_subs () {
     find "${__url_dir}" | grep -E '\.(aqt|cvd|dks|jss|mpl|txt|pjs|rt|smi|srt|ssa|ass|sub|idx|svcd|usf|psb|ttxt)$'   
 }
 
-# list_hashes 'sub name'
+# list_hashes 'Sub Name'
 list_hashes () {
     local __sub_title="${1}"
     local __sub_dir="${__download_dir_subs}/${__sub_title}"
     find "${__sub_dir}/" -maxdepth 1 -type d | sed "s#${__sub_dir}/##" | sort | uniq | sed '/^$/d'
 }
 
-# download_videos 'crunchy-name'
-download_videos () {
+# download_episodes 'crunchy-url-name'
+download_episodes () {
     local __video_title="${1}"
     local __video_dir="${__download_dir_videos}/${__video_title}"
     __mkdir "${__video_dir}"
-    pushd "${__video_dir}"
-    youtube-dl 'http://www.crunchyroll.com/'"${__video_title}" -u "${crunchy_email}" -p "${crunchy_password}" --all-subs --embed-subs --download-archive downloaded.txt --no-post-overwrites
-    popd
+    __pushd "${__video_dir}"
+    if ! [ -e '.downloaded' ]; then
+        youtube-dl 'http://www.crunchyroll.com/'"${__video_title}" -u "${crunchy_email}" -p "${crunchy_password}" --all-subs --embed-subs --download-archive .downloaded.txt --no-post-overwrites
+        find . | grep -E '\.mp4' | while read -r __file; do
+            ffmpeg -nostdin -i "${__file}" \
+            -vcodec copy \
+            -acodec copy \
+            -map 0 /tmp/output.mkv
+            mv /tmp/output.mkv "$(sed 's/\.mp4$//' <<< "${__file}").mkv"
+            rm "${__file}"
+        done
+        touch '.downloaded'
+    fi
+    __popd
 }
 
-# list_all_downloaded_subs 'sub name'
-list_all_downloaded_subs () {
+# list_all_subs 'Sub Name'
+list_all_subs () {
     local __sub_title="${1}"
     list_hashes "${__sub_title}" | while read -r __hash; do
-        list_downloaded_subs "${__sub_title}" "${__hash}"
+        list_downloaded_subs "${__sub_title}" "${__hash}" | sort -V
     done
+}
+
+# list_all_episodes 'crunchy-url-name'
+list_all_episodes () {
+    local __video_title="${1}"
+    local __video_dir="${__download_dir_videos}/${__video_title}"
+    find "${__video_dir}" | grep -E '\.mkv$' | sort -V
+}
+
+# assign_subs 'crunchy-url-name' 'Sub Name'
+assign_subs () {
+    local __video_title="${1}"
+    local __video_dir="${__download_dir_videos}/${__video_title}"
+    local __processed_file="${__video_dir}/.subbed"
+    touch "${__processed_file}"
+    local __sub_title="${2}"
+    local __sub_dir="${__download_dir_subs}/${__sub_title}"
+
+    local n=0
+
+    while read -r __video_file; do
+
+        ((n++))
+
+        if grep -Fx "${__video_file}" < "${__processed_file}" &> /dev/null; then
+            continue
+        fi
+
+        local __sub_list="$(list_all_subs "${__sub_title}")"
+
+        local __matching_sub_list="$(
+
+        # All possible matching patterns to find sub files.
+        grep " 0*${n} " <<< "${__sub_list}"
+        grep -E "(E|e)(P|p)0*${n}_" <<< "${__sub_list}"
+
+        )"
+
+        table=()
+
+        while read -r __matching_sub; do
+
+            table+=(TRUE "${__matching_sub}")
+
+        done <<< "${__matching_sub_list}"
+
+        __subs=()
+
+        while read -r __sub_file; do
+            __subs+=('--language' '0:eng' "${__sub_file}" )
+        done < <(
+            if ! ( [ "${zenity}" = 'false' ] || [ "${zenity}" = 'no' ] || [ "${zenity}" = '0' ] ); then
+                zenity --list --checklist \
+                --text="${__video_file}" \
+                --column="Select"  \
+                --column="File"  \
+                "${table[@]}" 2>/dev/null | sed 's/|/\n/'
+            else
+                echo "${__matching_sub_list}"
+            fi
+        )
+
+        echo "Adding subs to '$(basename "${__video_file}")'"
+        mkvmerge -o /tmp/output.mkv "${__video_file}" "${__subs[@]}" &> /dev/null
+        mv /tmp/output.mkv "${__video_file}"
+
+        echo "${__video_file}" >> "${__processed_file}"
+
+    done <<< "$(list_all_episodes "${__video_title}")"
+
 }
 
 __download_dir='downloads'
@@ -127,16 +221,17 @@ __mkdir "${__download_dir}"
 __show_file='shows.csv'
 __site_file='subs.html'
 
-curl 'http://kitsunekko.net/dirlist.php?dir=subtitles%2F' > "${__site_file}"
+echo 'Downloading Sub Page'
+wget 'http://kitsunekko.net/dirlist.php?dir=subtitles%2F' -qO - > "${__site_file}"
 
-awk-csv-parser -o '\n' "${__show_file}" | sed '/^$/d' | while mapfile -t -n 2 ary && ((${#ary[@]})); do
-    crunchy_title="${ary[0]}"
+awk-csv-parser -o '\n' "${__show_file}" | sed '/^$/d' | while mapfile -t -n 3 ary && ((${#ary[@]})); do
+    crunchy_url_name="${ary[0]}"
     sub_title="${ary[1]}"
-    echo "Crunchy Title: ${crunchy_title}"
+    echo "Crunchy Title: ${crunchy_url_name}"
     echo "Sub Title: ${sub_title}"
     download_subs "${sub_title}"
-    download_videos "${crunchy_title}"
-    list_all_downloaded_subs "${sub_title}"
+    download_episodes "${crunchy_url_name}"
+    assign_subs "${crunchy_url_name}" "${sub_title}"
 done
 
 exit
